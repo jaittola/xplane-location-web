6091590b646961abb15b12a80427ae5aaf519bad
Jukka Aittola <jukka.aittola@reaktor.com>
2020-10-24 20:38:58 +0300

    Update dependencies and remove unused request.js

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 const _       = require('lodash');
 var express = require("express");
-var request = require('request');
 
 var app  = express();
 var http = require("http").Server(app);
 var io   = require("socket.io")(http);
 
 var port = 3001;
-
+/*
 var mapsKey = process.env["GOOGLE_MAPS_KEY"];
 if (!mapsKey) {
     console.log("GOOGLE_MAPS_KEY missing form environment. Cannot start.");
     process.exit(1);
 }
+*/
 
 var udpreceive = require('./udpreceive');
 udpreceive.onData((message) => { io.emit('data', message); });
-
+/*
 io.on('connection', function(socket) {
     socket.emit('setup', { 'key': mapsKey });
 });
+*/
 
 app.use(express.static(__dirname + '/www')),
 
